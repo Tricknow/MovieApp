5a398e2a97a41af230f5f8cd80bca8f10460a669
Tricknow <argogy@naver.com>
2023-11-06 10:43:09 +0900

    add Router/Navigation

--- my_movie/src/App.js
+++ my_movie/src/App.js
@@ -3,10 +3,12 @@ import Home from "./routes/Home";
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css';
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
 function App(){
   return (
     <BrowserRouter>
+    <Navigation />
     <Routes>
       <Route element={<About/>} path='/:movieid'></Route>
       <Route element={<Home/>} path='/'></Route>
