04d72f7e120eba4f11dbfd70d4ab20d012754061
Tricknow <argogy@naver.com>
2023-11-12 14:42:33 +0900

    Array get 및디자인 수정

--- my_movie/src/routes/About.js
+++ my_movie/src/routes/About.js
@@ -4,23 +4,43 @@ import axios from 'axios'
 import { apiKey } from "../components/Env";
 import './About.css';
 
+const sample_logo = "kDedjRZwO8uyFhuHamomOhN6fzG.png"
 function About(){
     const param = useParams()
     const detailUrl = `https://api.themoviedb.org/3/movie/${param.movieid}?language=ko-ko&api_key=${apiKey}`;
     const [detail, setDetail] = useState("")
+    const [genre, setGenre] = useState("")
+    const [country, setCountry] = useState("")
+    const [company, setCompany] = useState("")
     useEffect(() => {
         axios({
             method: "GET",
             url: detailUrl
         })
         .then((res) => {           
-            //console.log(res)
+            //console.log(res.data)
             setDetail(res.data)
+            setGenre(res.data.genres)
+            setCountry(res.data.production_countries)
+            setCompany(res.data.production_companies)
         })
-    })
+    },[])
 
-    //const g_detail = detail.genres
-    //console.log(detail?.genre[0])
+    useEffect(() => {
+        //console.log(genre)
+    }, [genre]
+    )
+
+    useEffect(() => {
+        //console.log(country)
+    }, [country]
+    )
+
+    useEffect(() => {
+        console.log(company)
+    }, [company]
+    )
+
 
     return(
         <div class='about'>
@@ -33,8 +53,9 @@ function About(){
             <div class="detail">
                 <div class="detail_title">
                     <h3>{detail.title} ({detail.original_title})</h3>
-                    <h5>origin Country : detail.production_countries[0].name </h5>
-
+                    <h5>Origin country :{country.length >=1 ? country.map((e)=> (<span> [{e.name}] </span>)) : country}</h5>
+                    
+                     
                 </div>
                 <div class="detail_subClass">
                     <span role="img" aria-label="star">⭐{detail.vote_average} </span>
@@ -43,12 +64,14 @@ function About(){
                     <span>▪ </span>
                     <span>{detail.runtime}분 </span>
                     <span>▪ </span>
-                    <span>genres[0].name </span>
+                    {genre.length >=1 ? genre.map((e)=> (<span> [{e.name}]</span>)) : genre}
                 </div>
                 <p class="detail_overview">{detail.overview}</p>
             </div>
             <div>
-                <div><img src={`https://image.tmdb.org/t/p/original/${detail.production_companies}`} alt="Production_Company"/>detail.production_companies[0].name</div>
+                <div>제작사 ↓ 
+                    {company.length >=1 ? company.map((e)=> (<h5>{e.name} from {e.origin_country}</h5>)) : company}
+                </div>
                 
             </div>
         </div>
