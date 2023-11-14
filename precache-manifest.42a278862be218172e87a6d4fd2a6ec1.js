623ef2ab87c2f599ff58d69ae229d64d6729fc74
Tricknow <argogy@naver.com>
2023-11-14 18:49:11 +0900

    Updates

---
+++ precache-manifest.42a278862be218172e87a6d4fd2a6ec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20aa376270faef40c8c0628cc4b16206",
+    "url": "/MovieApp/index.html"
+  },
+  {
+    "revision": "01f88c6e77e70c429389",
+    "url": "/MovieApp/static/css/main.8eac7682.chunk.css"
+  },
+  {
+    "revision": "7ab3da5b60a43050cd7d",
+    "url": "/MovieApp/static/js/2.e2f19afb.chunk.js"
+  },
+  {
+    "revision": "01f88c6e77e70c429389",
+    "url": "/MovieApp/static/js/main.757c3563.chunk.js"
+  },
+  {
+    "revision": "f59b38517de36c5e1587",
+    "url": "/MovieApp/static/js/runtime~main.bc4dc0ff.js"
+  }
+]);
