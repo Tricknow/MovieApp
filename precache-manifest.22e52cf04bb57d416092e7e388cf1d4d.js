966e176984f8c9699b49e8b2d0c9071ff7052f00
Tricknow <argogy@naver.com>
2023-11-14 20:18:03 +0900

    Updates

--- precache-manifest.22e52cf04bb57d416092e7e388cf1d4d.js
+++ precache-manifest.22e52cf04bb57d416092e7e388cf1d4d.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "16cb806212856a49bf3be068d06a4556",
+    "revision": "eb870253f365765af8ea8490c4acc1a1",
     "url": "/MovieApp/index.html"
   },
   {
-    "revision": "ecb9f17a36d1ef9a65e1",
-    "url": "/MovieApp/static/css/main.6a79936b.chunk.css"
+    "revision": "799418520a103010f125",
+    "url": "/MovieApp/static/css/main.f84abccb.chunk.css"
   },
   {
     "revision": "7ab3da5b60a43050cd7d",
     "url": "/MovieApp/static/js/2.e2f19afb.chunk.js"
   },
   {
-    "revision": "ecb9f17a36d1ef9a65e1",
+    "revision": "799418520a103010f125",
     "url": "/MovieApp/static/js/main.757c3563.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MovieApp/precache-manifest.2786817cd89a4ffeeedfa9e84fbba000.js"
+  "/MovieApp/precache-manifest.22e52cf04bb57d416092e7e388cf1d4d.js"
 );
 
 self.addEventListener('message', (event) => {
